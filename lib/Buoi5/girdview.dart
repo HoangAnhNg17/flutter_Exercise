acf2f52fc9ec7b362aff87e2558a0a0405315da9
HoangAnhNg17 <hanhng1707@gmail.com>
2022-09-08 19:06:29 +0700

    zzz

--- lib/Buoi5/girdview.dart
+++ lib/Buoi5/girdview.dart
@@ -8,6 +8,7 @@ class Grid extends StatefulWidget {
 }
 
 class _GridState extends State<Grid> {
+  int selected = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,38 +27,52 @@ class _GridState extends State<Grid> {
       body: buildChess(9)
     );
   }
-}
-
-Widget buildChess(int n) {
-  return Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        GridView.builder(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          shrinkWrap: true,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: n,
-            childAspectRatio: 1
+  Widget buildChess(int n) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GridView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: n,
+                childAspectRatio: 1
+            ),
+            itemCount: n * n,
+            itemBuilder: (context, index) {
+              return (n % 2 != 0 && index % 2 == 0 ) || (n % 2 == 0 && ((index % 2 == 0 && (index ~/ n) % 2 == 0) || (index % 2 != 0 && (index ~/ n) % 2 != 0))) ? Black(index :index, n: n) : White(index: index, n: n);
+            },
           ),
-          itemCount: n * n,
-          itemBuilder: (context, index){
-            return (n % 2 != 0 && index % 2 == 0 ) || (n % 2 == 0 && ((index % 2 == 0 && (index ~/ n) % 2 == 0) || (index % 2 != 0 && (index ~/ n) % 2 != 0))) ? Black() : White();
-          },
-        ),
-      ],
-    ),
-  );
-}
+        ],
+      ),
+    );
+  }
+  Widget Black({required index, required n}) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          print('Row ${(index ~/ n) + 1} , Col ${(index % n)}');
+        });
+      },
+      child: Container(
+        color: Colors.black,
+      ),
+    );
+  }
 
-Widget Black() {
-  return Container(
-    color: Colors.black,
-  );
-}
+  Widget White({required index, required n}) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          print('Row ${(index ~/ n) + 1} , Col ${(index % n) +1}');
+        });
+      },
+      child: Container(
+        color: Colors.white,
+      ),
+    );
+  }
 
-Widget White() {
-  return Container(
-    color: Colors.white,
-  );
+
 }
